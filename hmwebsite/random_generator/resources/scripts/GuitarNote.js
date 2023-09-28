6e28f1c21f6581f62035fc329256ee35dd74fd8a
hmontaz <h.montaz.ca@gmail.com>
2023-09-28 01:48:23 -0700

    guitar neck tool initial commit

---
+++ hmwebsite/random_generator/resources/scripts/GuitarNote.js
@@ -0,0 +1,64 @@
+﻿/// <reference path="guitarneck.js" />
+
+class GuitarNote {
+	constructor(guitarNeck, stringNumber, fretNumber) {
+		this.guitarNeck = guitarNeck;
+		this.stringNumber = stringNumber;
+		this.fretNumber = fretNumber;
+		//--------------
+		this.initialize()
+	}
+	initialize() {
+		let neck = this.guitarNeck;
+		let o = neck.options;
+		let size = (neck.getStringSpace() - o.fretWidth) * .9
+		let fretIndex = this.fretNumber - 1;
+		let x = neck.getFretCX(fretIndex) + neck.getFretWidth(fretIndex) / 2 - neck.getNeckWidth() / 2;
+		let y = neck.getStringCY(this.stringNumber - 1) - neck.getNeckHeight() / 2
+
+		this.group = neck.newElement('g')
+			.appendTo(neck.svg)
+			.attr({
+				transform: `translate(${x},${y})`,
+			});
+		this.circleElement = neck.newElement('circle')
+			.css({
+				fill: '#ddd',
+				opacity: .5,
+				'stroke-width': .8,
+				stroke: 'black',
+			})
+			.attr({
+				cx: '50%',
+				cy: '50%',
+				r: size * .6,
+			}).appendTo(this.group);
+		this.textElement = neck.newElement('text')
+			.attr({
+				x: '50%',
+				y: '50%',
+				'dy': '0.3em',
+				'text-anchor': 'middle',
+				textLength: size,
+			})
+			.css({
+				'font-size': size * .7
+			})
+			.appendTo(this.group);
+
+		this.note = neck.addSemitones(o.openStrings[this.stringNumber - 1], this.fretNumber);
+		this.setText(this.note);
+	}
+	setText(text) {
+		this.text = text
+		this.textElement.html(text.replace('#', '♯').replace('b', '♭'))
+	}
+	remove() {
+		this.circleElement.remove()
+		this.textElement.remove()
+	}
+	hide() {
+		this.circleElement.hide()
+		this.textElement.hide()
+	}
+}
---
+++ hmwebsite/random_generator/resources/scripts/RandomGenerator.js
@@ -0,0 +1,45 @@
+﻿/// <reference path="jquery_3.2.1_jquery.js" />
+/// <reference path="guitarneck.js" />
+
+class RandomGenerator {
+	constructor(numberOfStrings, numberOfFrets) {
+		this.numberOfStrings = numberOfStrings;
+		this.numberOfFrets = numberOfFrets;
+	}
+	getRange(start, stop, step) {
+		step = step || 1;
+		return Array.from(
+			{ length: (stop - start) / step + 1 },
+			(value, index) => start + index * step
+		);
+	}
+	getRandom(max) {
+		return Math.floor(Math.random() * max);
+	}
+	generate(o) {
+		//console.log(o);
+		var strings = this.getRange(1, this.numberOfStrings);
+		var frets = this.getRange(1, this.numberOfFrets);
+		//var string_number = strings[this.getRandom(strings.length)];
+		//var string_number = 4;
+		//var fret_number = frets[this.getRandom(frets.length)];
+		//var fret_number = 3;
+		//console.clear();
+		/*console.log(
+			`%c${string_number} @ ${fret_number}`,
+			"font-family:sans-serif; font-size: 40px"
+		);*/
+		//https://www.petercollingridge.co.uk/tutorials/svg/interactive/javascript/
+		let result = null;
+		while (result == null) {
+			result = {
+				//stringNumber: strings[this.getRandom(strings.length)],
+				//fretNumber: frets[this.getRandom(frets.length)],
+				stringNumber: o.stringNumbers[this.getRandom(o.stringNumbers.length)],
+				fretNumber: o.fretNumbers[this.getRandom(o.fretNumbers.length)],
+			}
+			//if (predicate && !predicate(result)) result = null;
+		};
+		return result;
+	}
+}
