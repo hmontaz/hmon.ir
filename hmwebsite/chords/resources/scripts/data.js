2efeb769de632689461bb5e52c0b4cb268ba6707
hmontaz <h.montaz.ca@gmail.com>
2024-02-25 12:00:48 -0800

    added drop 2 & 3 to chords

--- hmwebsite/chords/resources/scripts/data.js
+++ hmwebsite/chords/resources/scripts/data.js
@@ -100,6 +100,41 @@ var data =
 			"title": "Drop 3 Diminished 7 Chords"
 		},
 		{
+			"category": "Drop 2 & 3",
+			"sortOrder": "1",
+			"name": "R△",
+			"path": "resources/images/drop2_3/RMaj7.svg",
+			"title": "Drop 2 & 3 Major 7 Chords"
+		},
+		{
+			"category": "Drop 2 & 3",
+			"sortOrder": "2",
+			"name": "R7",
+			"path": "resources/images/drop2_3/R7.svg",
+			"title": "Drop 2 & 3 7 Chords"
+		},
+		{
+			"category": "Drop 2 & 3",
+			"sortOrder": "3",
+			"name": "Rm7",
+			"path": "resources/images/drop2_3/Rm7.svg",
+			"title": "Drop 2 & 3 Minor 7 Chords"
+		},
+		{
+			"category": "Drop 2 & 3",
+			"sortOrder": "4",
+			"name": "Rm7♭5",
+			"path": "resources/images/drop2_3/Rm7b5.svg",
+			"title": "Drop 2 & 3 Minor 7 ♭5 Chords"
+		},
+		{
+			"category": "Drop 2 & 3",
+			"sortOrder": "5",
+			"name": "Rdim7",
+			"path": "resources/images/drop2_3/Rdim7.svg",
+			"title": "Drop 2 & 3 Diminished 7 Chords"
+		},
+		{
 			"category": "Drop 2 & 4",
 			"sortOrder": "1",
 			"name": "R△",
