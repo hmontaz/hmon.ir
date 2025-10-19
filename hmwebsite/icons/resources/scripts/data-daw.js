c45041958b88c0a7cc2d2a0c7ad481b60518e1cd
Hossein Montazeri <h.montaz.ca@gmail.com>
2025-10-19 13:19:39 -0700

    - added daw icons - added more blackfolder icons

---
+++ hmwebsite/icons/resources/scripts/data-daw.js
@@ -0,0 +1,117 @@
+﻿var icons = icons || {};
+icons['daw'] = [
+	{
+		"filename": "toontracks-SD3"
+	},
+	{
+		"filename": "voice recorder"
+	},
+	{
+		"filename": "voice-recorder"
+	},
+	{
+		"filename": "warning"
+	},
+	{
+		"filename": "drums"
+	},
+	{
+		"filename": "edit"
+	},
+	{
+		"filename": "guitar-acoustic"
+	},
+	{
+		"filename": "guitar-acoustic-group"
+	},
+	{
+		"filename": "guitar-acoustic-monitored"
+	},
+	{
+		"filename": "guitar-bass"
+	},
+	{
+		"filename": "guitar-bass-group"
+	},
+	{
+		"filename": "guitar-bass-monitored"
+	},
+	{
+		"filename": "guitar-lead"
+	},
+	{
+		"filename": "guitar-lead-group"
+	},
+	{
+		"filename": "guitar-lead-monitored"
+	},
+	{
+		"filename": "guitar-rhythm"
+	},
+	{
+		"filename": "guitar-rhythm-group"
+	},
+	{
+		"filename": "guitar-rhythm-monitored"
+	},
+	{
+		"filename": "idea"
+	},
+	{
+		"filename": "line6 helix"
+	},
+	{
+		"filename": "line6 helix-round"
+	},
+	{
+		"filename": "metronome"
+	},
+	{
+		"filename": "microphone"
+	},
+	{
+		"filename": "microphone-condenser"
+	},
+	{
+		"filename": "microphone-condenser2"
+	},
+	{
+		"filename": "microphone-dynamic"
+	},
+	{
+		"filename": "note-eight"
+	},
+	{
+		"filename": "notes"
+	},
+	{
+		"filename": "rock-on"
+	},
+	{
+		"filename": "sparks"
+	},
+	{
+		"filename": "speaker"
+	},
+	{
+		"filename": "speaker-sub"
+	},
+	{
+		"filename": "speech"
+	},
+	{
+		"filename": "synth"
+	},
+	{
+		"filename": "synth-2"
+	},
+	{
+		"filename": "toontracks-EZBass"
+	},
+	{
+		"filename": "toontracks-EZD"
+	},
+	{
+		"filename": "toontracks-EZKeys2"
+	}
+];
--- hmwebsite/icons/resources/scripts/handler.js
+++ hmwebsite/icons/resources/scripts/handler.js
@@ -1,25 +1,53 @@
 ﻿var handler = {
-	init: function () {
-		var _this = this;
-		for (var i = 0; i < icons_list.length; i++) {
-			var item = icons_list[i];
-			var content = $('#content');
-			var url = 'resources/icons/' + item.filename + '.ico';
+	src: 'blackfolder',
+	icons: [],
+	getFileExtension: function () {
+		switch (this.src) {
+			case 'blackfolder':
+				return '.ico';
+			case 'daw':
+			default:
+				return '.png';
+		}
+	},
+	init: function (src, icons) {
+		let _this = this;
+		_this.src = src;
+		_this.icons = icons;
+		//----------- Sort icons alphabetically
+		_this.icons.sort(function (a, b) {
+			let nameA = a.filename.toUpperCase(); // ignore upper and lowercase
+			let nameB = b.filename.toUpperCase(); // ignore upper and lowercase
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			// names must be equal
+			return 0;
+		});
+		//----------- Display icons
+		for (var i = 0; i < _this.icons.length; i++) {
+			let item = _this.icons[i];
+			let content = $('#content');
+			let url = `resources/icons-${src}/${item.filename}${_this.getFileExtension()}`;
 
-
-			var img = $('<img>')
+			let img = $('<img>')
 				.attr('src', url)
 				.css({ 'width': '128px' });
-			var s = item.description || item.filename.toCamelCase();
-			var text = $('<div>').html(s);
+			let s = item.description || item.filename.toCamelCase();
+			let text = $('<div>').html(s);
+			let imgContainer = $('<div>').addClass('img-container');
+			imgContainer.append(img);
 
-			var element = $('<dvi>').appendTo(content)
-				.append(img)
+			let element = $('<div>').appendTo(content)
+				.append(imgContainer)
 				.append(text)
 				.addClass('icon-thumbnail')
 				.attr('download_url', url)
-				.attr('download_filename', item.filename + '.ico')
-				.click(function () {
+				.attr('download_filename', item.filename + _this.getFileExtension())
+				.on('click', function () {
 					var url = $(this).attr('download_url');
 					var filename = $(this).attr('download_filename');
 					_this.download(url, filename);
